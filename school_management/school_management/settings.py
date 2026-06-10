c693a34af4d3c0c3dda8c32f919e7c8a0b8a16cc
Jishnu Gopikrishnan <jishnugopikrishnan0@gmail.com>
2026-06-11 02:24:46 +0530

    Initial Commit 3

--- school_management/school_management/settings.py
+++ school_management/school_management/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-@w-pu1ev_j=p7c9&a*itx^19$6myls%&=2on31!*pzwl%74^e8'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
